ff2fa959c049b1e8e9448793333c75640356c035
Andrei Martillo <ddrhckrzz@gmail.com>
2026-02-07 07:53:06 +0800

    Assignment 1 done.

--- index.php
+++ index.php
@@ -16,6 +16,10 @@ if ($mysqli->connect_error) {
     echo "Connected to MariaDB via Socket: " . $socket_path;
 }
 
+echo "<hr>";
+echo "<h2>Table of Contents</h2><br>";
+echo "<a href='ass1/index.html'>Go to Assignment 1</a>";
+
 // 3. Show full PHP Info
 echo "<hr>";
 phpinfo();
